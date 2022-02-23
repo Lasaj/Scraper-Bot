f15e4be95fa819d8f030f4e30eecffec254f135f
Rick <rick.wainwright2@gmail.com>
2022-02-23 22:12:40 +1000

    clean comments

--- bot.py
+++ bot.py
@@ -78,9 +78,6 @@ async def on_message(message):
                     limit=limit + 1000):  # The added 1000 is so in case it skips messages for being
                 if msg.author != client.user:  # a command or a message it sent, it will still read the
                     if not is_command(msg):  # the total amount originally specified by the user.
-                        # data = data.append({'content': msg.content,
-                        #                     'time': msg.created_at,
-                        #                     'author': msg.author.name}, ignore_index=True)
                         row = pd.DataFrame([[msg.content, msg.created_at, msg.author.name]], columns=['content', 'time', 'author'])
                         data = pd.concat([data, row], ignore_index=True)
                     if len(data) == limit:
